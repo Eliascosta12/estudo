79288f9ff129609b47a83f31cc0871a7b15d0122
Eliascosta12 <eliascostahh12@gmail.com>
2025-03-26 01:11:56 +0000

    trabalho supertrunfo

---
+++ supertrunfo.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+
+int main(){
+
+char estadoc1; // %c
+char nomecidadec1 [100]; // %s
+char codcarta1 [100] ; // %s 
+int populacao1; // %i
+float area1; // %f
+float pib1;// %f
+int pturistico1; //%d
+
+
+
+
+printf("carta 1:\n");
+
+
+printf("Estado:");
+scanf("\n %c", &estadoc1);
+
+printf("Código:");
+scanf("\n %s", &codcarta1);
+
+printf("Nome da Cidade:");
+scanf("\n %s", &nomecidadec1);
+
+printf("População :");
+scanf("\n %i", &populacao1);
+
+printf("Área :");
+scanf("\n %f", &area1);
+
+printf("PIB :");
+scanf("\n %f", &pib1);
+
+printf("Número de Pontos Turísticos:");
+scanf("\n %d", &pturistico1);
+
+
+printf("carta 1:\n");
+printf("Estado: %c\n", estadoc1);
+printf("Codigo : %s \n ", codcarta1);
+printf("Nome da Cidade: %s \n ", nomecidadec1);
+printf("População: %i \n " , populacao1);
+printf("Área: %f \n", area1);
+printf("PIB: %f \n", pib1);
+printf("Número de Pontos Turísticos: %d \n",pturistico1);
+    
+
+printf("carta 2:\n");
+
+printf("Estado:");
+scanf("\n %c", &estadoc1);
+
+printf("Código:");
+scanf("\n %s", &codcarta1);
+
+printf("Nome da Cidade:");
+scanf("\n %s", &nomecidadec1);
+
+printf("População :");
+scanf("\n %i", &populacao1);
+
+printf("Área :");
+scanf("\n %f", &area1);
+
+printf("PIB :");
+scanf("\n %f", &pib1);
+
+printf("Número de Pontos Turísticos:");
+scanf("\n %d", &pturistico1);
+
+
+printf("carta 2:\n");
+printf("Estado: %c\n", estadoc1);
+printf("Codigo : %s \n ", codcarta1);
+printf("Nome da Cidade: %s \n ", nomecidadec1);
+printf("População: %i \n " , populacao1);
+printf("Área: %f \n", area1);
+printf("PIB: %f \n", pib1);
+printf("Número de Pontos Turísticos: %d \n",pturistico1);
+
+
+
+
+
+
+
+
+return 0;
+
+
+
+
+}
